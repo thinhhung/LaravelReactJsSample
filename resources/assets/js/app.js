29018c1883c14c72cb87fbeb41301463ae816915
Thinh Hung <hungnt.dev@gmail.com>
2018-03-29 23:33:14 +0700

    Add users module (CRUD)

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,13 +4,28 @@
  * includes React and other helpers. It's a great starting point while
  * building robust, powerful web applications using React + Laravel.
  */
-
-require('./bootstrap');
-
+import React from 'react'
+import { render } from 'react-dom'
+import {
+  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
+import CreateUser from './components/CreateUser'
+import EditUser from './components/EditUser'
+import UserList from './components/UserList'
 /**
  * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-require('./components/Example');
+const history = createBrowserHistory()
+render (
+  <Router history={history}>
+    <Switch>
+      <Route path='/users/create' component={CreateUser} />
+      <Route path='/users/edit/:id' component={EditUser} />
+      <Route path='/' component={UserList} />
+    </Switch>
+  </Router>, document.getElementById('app'))
--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'api'], function () {
+    Route::resource('users', 'UserController');    
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::view('/{any}', 'welcome')
+    ->where('any', '.*');
